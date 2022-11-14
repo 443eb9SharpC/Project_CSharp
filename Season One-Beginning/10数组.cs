042085b00ca28ebcd098f8d581d6ac281b0ef5b0
443eb9#C <3166943013@qq.com>
2022-11-14 23:17:10 +0800

    11.14 Learned

--- Season One-Beginning/10数组.cs
+++ Season One-Beginning/10数组.cs
@@ -6,7 +6,43 @@ namespace Season_One_Beginning
     {
         static void Main(string[] args)
         {
+            string[] name = { "aaa", "bbb", "ccc", "ddd" };
 
+            for (int i = 0; i < name.Length; i++)
+            {
+                Console.Write(name[i] + " ");
+            }
+            Console.WriteLine();
+
+            //foreach遍历
+            //即取出每一个name[]中的值赋给temp
+            foreach (string temp in name)
+            {
+                Console.Write(temp + " ");
+            }
+            Console.WriteLine();
+
+            int[] age;
+            age = new int[5]; //以默认值赋值
+
+            string str1 = " aaa bbb ";
+            string str2 = str1.Trim();  //去除字符串中的空格
+            string str3 = str1.TrimStart();  //去除开头的空格
+            string str4 = str1.TrimEnd();  //去除末尾的空格
+
+            Console.WriteLine("str1=" + str1);
+            Console.WriteLine("str2=" + str2);
+            Console.WriteLine("str3=" + str3);
+            Console.WriteLine("str4=" + str4);
+
+            string names = "aaa,bbb,ccc";
+            //以","分割字符串并输出至一个数组
+            string[] nameArr = names.Split(',');
+            foreach (string temp in nameArr)
+            {
+                Console.Write(temp + " ");
+            }
+            Console.WriteLine();
         }
     }
 }
---
+++ Season One-Beginning/11函数.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Season_One_Beginning
+{
+    class Program
+    {
+        static int Sum(int[] nums)
+        {
+            int sum = 0;
+            foreach (int temp in nums)
+            {
+                sum += temp;
+            }
+            return sum;
+        }
+
+        //params关键字: 将传入的数字打包成数组
+        static int AdvancedSum(params int[] nums)
+        {
+            int sum = 0;
+            foreach (int temp in nums)
+            {
+                sum += temp;
+            }
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            int aaa = Sum(new int[] { 1, 2, 3, 4, 5, 6 });  //必须new int[],不能传入{1,2,3,4,5,6}
+            Console.WriteLine(aaa);
+
+            int bbb = AdvancedSum(1, 2, 3, 4, 5, 6, 7, 8, 9);
+            Console.WriteLine(bbb);
+        }
+    }
+}
