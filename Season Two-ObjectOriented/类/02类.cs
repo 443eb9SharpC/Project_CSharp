846ed27224c881108d2d816aeebad1e616520c55
443eb9#C <3166943013@qq.com>
2022-11-16 23:41:56 +0800

    11.16 Learned

--- Season Two-ObjectOriented/类/02类.cs
+++ Season Two-ObjectOriented/类/02类.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using Season_Two_ObjectOriented.类;
+using System;
 
-namespace Season_One_Beginning
+namespace Season_Two_ObjectOriented
 {
     class Program02
     {
         static void Main(string[] args)
         {
-
+            Customer customerA = new Customer(001, "张三", 30, 2020);
+            
+            customerA.Age = 30;  //属性可以直接拿来修改
         }
     }
 }
--- Season Two-ObjectOriented/类/Customer.cs
+++ Season Two-ObjectOriented/类/Customer.cs
@@ -8,18 +8,17 @@ namespace Season_Two_ObjectOriented.类
 {
     internal class Customer
     {
-        private int m_id;
-        private string m_name;
-        private int m_age;
-        private int m_joinYear;
+        public int m_id;
+        public string m_name;
+        public int m_age;
+        public int m_joinYear;
 
         public int Age //属性
         {
             get { return m_age; }
             set { this.m_age = value; }  //默认会将传入的参数赋值给value
         }
-
-        Customer(int id,string name,int age,int joinYear)  //构造函数
+        public Customer(int id, string name, int age, int joinYear)  //构造函数
         {
             this.m_id = id;
             this.m_name = name;
