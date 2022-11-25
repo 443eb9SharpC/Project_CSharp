57bc80e633b4267824b8ab86c143b0be578f331d
443eb9#C <3166943013@qq.com>
2022-11-25 23:40:54 +0800

    11.25 Learned

---
+++ Season Two-ObjectOriented/05static静态/05static静态.cs
@@ -0,0 +1,15 @@
+﻿using Season_Two_ObjectOriented._05static静态;
+using System;
+
+namespace Season_Two_ObjectOriented
+{
+    class Program02
+    {
+        static void Main(string[] args)
+        {
+            Test t1= new Test();
+            //t1.EgFunc();  //静态的东西用类名访问
+            Test.EgFunc();
+        }
+    }
+}
---
+++ Season Two-ObjectOriented/05static静态/Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Season_Two_ObjectOriented._05static静态
+{
+    internal class Test
+    {
+        //static的东西是公用的
+        public static int speed;
+        public int hp;
+        public static void EgFunc()
+        {
+            //静态函数只能使用静态变量
+            Console.WriteLine(speed);
+        }
+    }
+}
